da568ef25f1ae5f83b888295ed6f72495800c55e
miltonfranciscopf <miltonfranciscopf9@gmail.com>
2024-09-08 22:16:20 +0000

    tema 4 mestre batalha naval imprimindo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,46 +1,13 @@
 #include <stdio.h>
 
-
-    // Desafio Batalha Naval - MateCheck
-    // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-    // Siga os comentários para implementar cada parte do desafio.
-#define POSICAOX 10
+#define POSICAOX 10 //X e Y linha e coluna
 #define POSICAOY 10
-//X e Y linha e coluna 
-
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-    
  
+void metodoCone(){
+    
+}
+int main() {    
+    int opcao=0; //opcao do menu
     int tabuleiro[POSICAOX][POSICAOY]={
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0},
@@ -53,7 +20,7 @@ int main() {
         {0,0,0,0,0,0,0,0,0,0},
         {0,0,0,0,0,0,0,0,0,0}
     };
-    
+    //posicionar navios
     for (int i = 0; i < POSICAOX; i++)
     {
        for (int j = 0; j < POSICAOY; j++)
@@ -83,8 +50,8 @@ int main() {
 
        }
     }
-
-    //imprimir
+    
+    //imprimir navios
     for (int k = 0; k < POSICAOX; k++)
     {
        for (int m = 0;  m< POSICAOY; m++)
@@ -93,6 +60,188 @@ int main() {
        }
         printf("\n");
     }
+    printf("\n");
+    printf("MENU\n");
+    printf("1- Habilidade em Cone\n");
+    printf("2- Habilidade em Octaedro\n");
+    printf("3- Habilidade em Cruz\n");
+    printf("4- Sair\n");
+    
+    do
+    {
+        int tabuleiroDois[POSICAOX][POSICAOY]={
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}
+        };
+        int cont=0; //contadores para controle
+        int contadorDois=0;
+        printf("Digite a opcao: ");
+        scanf("%d",&opcao);
+        printf("\n");
+        switch (opcao)
+        {
+        case 1:
+            
+            //habilidade de cone 
+            for (int i = 0; i < POSICAOX; i++)
+            {
+                for (int j = 0; j < POSICAOY; j++)
+                {
+                    cont = i;
+                    contadorDois=i;
+                    if(cont==i&& j>3 && j<5+cont){
+                        tabuleiroDois[i][j]=1; 
+                        cont++;            
+                    } 
+                    if(j<4 && j>3-contadorDois && contadorDois==i){
+                        tabuleiroDois[i][j]=1;
+                        contadorDois++;
+                    }
+                    
+                }
+                
+            }
+            
+            for (int q = 0; q < POSICAOX; q++)
+            {
+                for (int r = 0; r < POSICAOY; r++)
+                {
+                    printf("  %d",tabuleiroDois[q][r]);   
+                }
+                
+                printf("\n");
+            }
+            break;
+        case 2:
+            //habilidade de octaedro
+            cont=0; //contadores para controle
+            contadorDois=0;
+            int contadorTres=0;;
+            for (int i = 0; i < POSICAOX; i++)
+            {
+                if(i>5){
+                    
+                    contadorTres++;
+                }
+                for (int j = 0; j < POSICAOY; j++)
+                {
+                    cont = i;
+                    contadorDois=i;           
+
+                    if(cont==i&& j>3 && j<5+cont && i<5){
+                        tabuleiroDois[i][j]=1; 
+                        cont++;            
+                    } 
+                    if(contadorDois==i && j<5 && j>4-contadorDois && i<5){
+                        tabuleiroDois[i][j]=1;
+                        contadorDois++;
+                    }
+                    if(j>0+contadorTres && j<5 && i>4){
+                        tabuleiroDois[i][j]=1;
+                    }
+                    if(j>3 && j<9-contadorTres && i>4){
+                        tabuleiroDois[i][j]=1;            
+                    } 
+                    
+                    
+                }
+                
+            }
+            
+            for (int q = 0; q < POSICAOX; q++)
+            {
+                for (int r = 0; r < POSICAOY; r++)
+                {
+                    printf("  %d",tabuleiroDois[q][r]);   
+                }
+                
+                printf("\n");
+            }    
+            break;
+        case 3:
+            //habilidades de cruz 
+            for (int n = 0; n < POSICAOX; n++)
+            {
+                for (int p = 0; p < POSICAOY; p++)
+                {
+                    if(p>4 && p<6){
+                        tabuleiroDois[n][p]=1; 
+                    }
+                    if(n==5 && p>0 && p<9){
+                        tabuleiroDois[n][p]=1; 
+                    }
+                        
+                }
+            }
+            //imprimir habilidade
+            for (int q = 0; q < POSICAOX; q++)
+            {
+                for (int r = 0; r < POSICAOY; r++)
+                {
+                    printf("  %d",tabuleiroDois[q][r]);   
+                }
+                
+                printf("\n");
+            }
+            break;
+        
+        default:
+            printf("Opcao Invalida,tente novamente.");
+            break;
+        }
+
+        //imprimir efeito da habilidade nos navios
+        int tabuleiroTres[POSICAOX][POSICAOY]={
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}
+        };
+
+        for (int i = 0; i < POSICAOX; i++)
+        {
+            for (int j = 0; j < POSICAOY; j++)
+            {
+                if(tabuleiro[i][j]==3){
+                    tabuleiroTres[i][j]=3;
+                }
+                if(tabuleiro[i][j]==3 && tabuleiroDois[i][j]==1){
+                    tabuleiroTres[i][j]=1;
+                }
+                
+            }
+            
+        }
+        //imprimir resultado o efeito da habilidade nos navios
+        printf("\n");
+        for (int q = 0; q < POSICAOX; q++)
+        {
+            for (int r = 0; r < POSICAOY; r++)
+            {
+                printf("  %d",tabuleiroTres[q][r]);   
+            }
+                
+            printf("\n");
+        }
+        
+        
+    } while (opcao!=4);
+    
+
     
     return 0;
 }
